5f51c2b0d48c8706fff0155689ce5ae2f57c35e8
FernHerrera11 <138176983+FernHerrera11@users.noreply.github.com>
2025-01-30 21:20:17 -0800

    Add files via upload

---
+++ src/Containers/App.js
@@ -0,0 +1,71 @@
+import React, {Component} from 'react';
+import CardList from '../Components/CardList';
+// import { robots } from './robots';
+import SearchBox from '../Components/SearchBox';
+import Scroll from '../Components/Scroll';
+// import './App.css';
+
+
+
+
+//Creating a class fuctions, that extends the index.js root variable
+//The extension creates a constructor to create state on the main 
+//component for the App.js
+class App extends Component {
+	constructor() {
+		super()
+		this.state = {
+			robots: [],
+			searchfield: ''
+		}  					 
+
+	}
+//componentDidMount calls it after the component is added to the screen
+//Common place to start data fetching, set subscriptions, manipulate the DOM nodes. 
+	//Have to impliment componentWillUnmount, and componentDidUpdate
+	//These are life cycle methods to avoid bugs, with state, and props
+	componentDidMount() { //componentDidMount doesnt take parameters. 
+		fetch('https://jsonplaceholder.typicode.com/users')
+		.then(response => response.json())
+		.then(users => this.setState({robots: users}));
+		//componentDidMount Should not return anything.    				   
+	}
+// function what takes in an event to see changes in search bar
+	onSearchChange = (event) => {
+		//sets state(to change the searchfield to the eventtargetvalue)
+		this.setState({searchfield: event.target.value });
+	
+
+	}
+
+	//Renders this to the page. 
+	render(){
+//returns filters robots list from API and sets it to filteredRObots
+			const { robots, searchfield} = this.state;
+			const filteredRobots = robots.filter(robot => {
+			return robot.name.toLowerCase().includes(searchfield.toLowerCase());
+		})
+			
+		if (!robots.length){
+				return <h1> Loading ..... </h1>
+			}else{
+				return(
+ 
+					<div className='tc'>
+					<h1> RoboFriends </h1>
+					  <SearchBox searchChange={this.onSearchChange} />
+					  <Scroll>
+						<CardList robots={filteredRobots} />
+					  </Scroll>
+					</div>
+			
+					);
+			
+			}
+
+	
+	}
+	
+
+}
+export default App;
---
+++ src/index.js
@@ -0,0 +1,27 @@
+//imports are important to import other files in same folder
+//imports dependencies to use extra functions, code
+//Able to destructure the files. 
+
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+//import Card from './Card';
+import App from './Containers/App';
+import 'tachyons';
+import reportWebVitals from './reportWebVitals';
+
+//root is used to start the react app, getElement'Root'
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+
+//root.rendering the components to the web. 
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
